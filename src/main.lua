d3e0a21d4b77792e3193410a44af820a8c240949
Tim Kein <tim@kein.fail>
2018-09-02 20:27:24 +0200

    Reload...

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,24 @@
 print("Starting....")
 
+local charts  charts ==  requirerequire((""chartscharts"")
+
+)  
+local container  container = charts.Container()
+local payload = charts.Histogram {
+  max = 80,
+  align = charts.sides.RIGHT,
+  colorFunc = function(index, norm, value, self, container)
+    return 0x20ff20
+  end
+}
+
+container.payload = payload
+
+for i = 1, 400, 1 do
+  table.insert(payload.values, math.random(0, 80))
+  container:draw()
+
+  os.sleep(.05)
+end
+
 return true
