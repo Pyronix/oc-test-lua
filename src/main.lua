d419bfa7b8155880062e3806e78c1b5526a8ac1e
Tim Kein <tim@kein.fail>
2018-09-02 20:35:34 +0200

    Reload...

--- src/main.lua
+++ src/main.lua
@@ -4,19 +4,24 @@ local charts = require("charts")
 
 local container = charts.Container()
 local payload = charts.Histogram {
-  max = 80,
+  max = 100,
   align = charts.sides.RIGHT,
   colorFunc = function(index, norm, value, self, container)
     return 0x20ff20
   end
 }
 container.payload = payload
+ 
+local current, current_max, p
 
-for i = 1, 400, 1 do
-  table.insert(payload.values, math.random(0, 80))
-  container:draw()
+while true do
+	current = component.energy_device.getEnergyStored()
+	current_max = component.energy_device.getMaxEnergyStrored()
 
-  os.sleep(.05)
+	p = current / current_max * 100
+
+	table.insert(payload.values, p)
+	os.sleep(1)
 end
 
 return true
