f5eca25888f9722bee04d068ff05fed23802e992
Tim Kein <tim@kein.fail>
2018-09-02 19:02:00 +0200

    Reload...

--- oc-test.lua
+++ oc-test.lua
@@ -1,1 +1,30 @@
-print("Hello World!")
+local component = require("component")
+local event = require("event")
+local fs = require("filesystem")
+local serial = require("serialization")
+local shell = require("shell")
+local term = require("term")
+
+local gpu = component.gpu
+
+local internet
+local wget
+
+local args, options = shell.parse(...)
+
+local function getInternet()
+  if not component.isAvailable("internet") then
+    io.stderr:write("This program requires an internet card to run.")
+    return false
+  end
+  internet = require("internet")
+  wget = loadfile("/bin/wget.lua")
+  return true
+end
+
+
+if not getInternet() then
+    os.exit()
+end
+
+print("Welcome!")
